7c92eecb774fa257504f371de68bed7c3ad81e78
Ilgu Lee <iglee2000ca@gmail.com>
2018-05-19 23:37:34 -0400

    Add adding using form&post, delete using ajax&delete

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const app = express();
 
 // MongoClient 
 const MongoClient = require('mongodb').MongoClient;
+const ObjectID=require('mongodb').ObjectID;
 const url = 'mongodb://localhost:27017/todoapp';
 
 //Body Parser Middleware
@@ -44,5 +45,30 @@ app.get('/', (req, res, next) => {
         res.render('index',{todos:todos});
     });
    
+});
+
+app.post('/todo/add',(req,res,next)=>{
+   const todo={
+       text:req.body.text,
+       body:req.body.body
+   }
+   Todos.insert(todo,(err,result)=>{
+    if(err){
+        return console.log(err);
+    }
+    console.log('Todo Added...');
+     res.redirect('/');
+   });
+});
+
+app.delete('/todo/delete/:id',(req,res,next)=>{
+    const query={_id:ObjectID(req.params.id)}
+    Todos.deleteOne(query,(err,response)=>{
+        if(err){
+            return console.log(err);
+        }
+        console.log('Todo Removed');
+        res.sendStatus(200);
+    });
 });
 
